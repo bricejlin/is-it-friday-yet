041f6311dbd9a279dbe57f09dcdabf11292b9fe5
Brice Lin <blin@spotify.com>
2019-04-25 23:24:32 -0400

    sdf

--- src/app.js
+++ src/app.js
@@ -5,8 +5,7 @@ app.get('/', (req, res) => {
   const isItTuesday = (new Date()).getDay() === 2
   res.send({
     response: isItTuesday ? 'Yaas' : 'Noo',
-    timestamp: Date.now(),
-    df: 'asdf'
+    timestamp: Date.now()
   })
 })
 
