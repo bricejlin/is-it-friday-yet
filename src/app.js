ba872b02a13ec3ced3cccd5778a9564ef324e7ba
Brice Lin <blin@spotify.com>
2019-04-26 22:46:37 -0400

    build docker

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const app = express()
 app.get('/', (req, res) => {
   const isItTuesday = (new Date()).getDay() === 2
   res.send({
-    response: isItTuesday ? 'Yaas' : 'Noo'
+    response: isItTuesday ? 'Yaas' : 'Noo',
+    asdf: 'sdf'
   })
 })
 
